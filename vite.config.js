509a48f1a3702d203285b364b205a4a9a766e2d4
AbijithT2003 <147176566+AbijithT2003@users.noreply.github.com>
2025-07-20 23:14:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/RoDAC/' ,
+  base: 'https:/abijitht2003.github.io/RoDAC/ ',
   plugins: [react()],
 })
