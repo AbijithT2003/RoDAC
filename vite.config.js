01244ddb7963f7802e1a2c70b51c6eb76e19f870
AbijithT2003 <jitthu186@gmail.com>
2025-07-20 23:40:54 +0530

    Changed yml and vite config base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https:/abijitht2003.github.io/RoDAC/ ',
+  base: '/RoDAC/',
   plugins: [react()],
 })
